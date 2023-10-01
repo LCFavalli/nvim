ec14aabede8a72ac795349a6ab982e44b0a74e6d
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2023-10-01 15:35:54 +0200

    Update TAB
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- lua/fcb/set.lua
+++ lua/fcb/set.lua
@@ -1,9 +1,10 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+local TAB = 2
+vim.opt.tabstop = TAB
+vim.opt.softtabstop = TAB
+vim.opt.shiftwidth = TAB
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
