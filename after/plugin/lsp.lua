b709dacefbabe2703e2a87cc7c026fbd18d8c1a0
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2024-01-31 15:38:14 +0100

    Update lsp
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -303,7 +303,7 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.lsp.start({
       name = 'statemachine',
       cmd = vim.lsp.rpc.connect('127.0.0.1', 5123),
-      root_dir = vim.fs.dirname(vim.fs.find({ 'start.lsp' }, { upward = true })[1]),
+      root_dir = vim.fs.dirname(vim.fs.find({ 'start.lsp', 'build.gradle' }, { upward = true })[1]),
       -- filetypes = { 'tst', 'nl },
     })
   end,
