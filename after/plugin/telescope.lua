0de59a758bf108e96380789afeb5f5ad3844e8e6
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2024-08-24 08:29:55 +0200

    Update telescope
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,7 +2,7 @@ local actions = require("telescope.actions")
 
 require("telescope").setup({
     defaults = {
-        disable_devicons = true,
+        -- disable_devicons = true,
         mappings = {
             i = {
                 ["<esc>"] = actions.close,
