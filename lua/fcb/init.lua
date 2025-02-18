d2c79784536a3920ff02f4be9c3200b4e2a04da0
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2025-02-18 12:14:35 +0100

    Update py
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- lua/fcb/init.lua
+++ lua/fcb/init.lua
@@ -3,7 +3,7 @@ require("fcb.remap")
 require("fcb.lazy")
 -- require("fcb.packer")
 
-vim.g.python3_host_prog = '/usr/bin/python3.6'
+vim.g.python3_host_prog = '/usr/bin/python3'
 
 local augroup = vim.api.nvim_create_augroup
 local fcb = augroup('fcb', {})
