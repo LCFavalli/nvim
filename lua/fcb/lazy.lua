9397845b791df1460b9232badb47e9ebd254846a
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2025-04-10 00:45:45 +0200

    Update md prev
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- lua/fcb/lazy.lua
+++ lua/fcb/lazy.lua
@@ -141,8 +141,11 @@ require("lazy").setup({
     {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && npm install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
         ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end,
     },
 
     -- COPILOT
